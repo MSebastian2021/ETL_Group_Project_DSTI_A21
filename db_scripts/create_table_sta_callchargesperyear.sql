feb2ff2dc34b72571bee9c4ee729459161b918ec
Maria Sebastian <mariarosariosebastian@gmail.com>
2022-06-09 12:25:32 +0200

    refactor: Update Call Charges packages and tables to new name CallChargesPerYear

--- db_scripts/create_table_sta_callchargesperyear.sql
+++ db_scripts/create_table_sta_callchargesperyear.sql
@@ -1,17 +1,17 @@
 USE [SSIS_PROJECT_STA]
 GO
 
-/****** Object:  Table [dbo].[CallCharges]    Script Date: 07/06/2022 08:51:59 ******/
+/****** Object:  Table [dbo].[CallChargesPerYear]    Script Date: 09/06/2022 12:22:25 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[CallCharges](
-	[Year] [nvarchar](255) NULL,
+CREATE TABLE [dbo].[CallChargesPerYear](
 	[CallTypeKey] [nvarchar](255) NULL,
 	[CallType] [nvarchar](255) NULL,
+	[Year] [nvarchar](255) NULL,
 	[CallChargesPerMin] [nvarchar](255) NULL
 ) ON [PRIMARY]
 GO
